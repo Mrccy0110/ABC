d472d9429e8fba30c7aa730a3854a36b5485885c
Mrccy0110 <90121460+Mrccy0110@users.noreply.github.com>
2025-12-10 16:52:25 +0800

    Update test.c

--- test.c
+++ test.c
@@ -170,6 +170,74 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* handler.c */
+
+#include "app_ctx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define ITEM_SIZE 32
+
+extern int g_backend_fd;
+
+int handle_client(int sock_fd) {
+    app_ctx_t app;
+    app_ctx_init(&app, sock_fd);
+
+    int limit = get_client_limit(&app);
+
+    size_t total = (size_t)limit * ITEM_SIZE;
+    char *buf = (char *)malloc(total);
+    if (!buf) {
+        const char *resp = "ERR no memory\n";
+        (void)write(sock_fd, resp, strlen(resp));
+        return -1;
+    }
+
+    size_t off = 0;
+    for (int i = 0; i < limit; ++i) {
+        ssize_t n = read(g_backend_fd, buf + off, ITEM_SIZE);
+        if (n <= 0) {
+            free(buf);
+            return -1;
+        }
+        off += (size_t)ITEM_SIZE;
+    }
+
+    free(buf);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
 int handle_request(int fd) {
     char recv_buf[512];
     char user_msg[128];
